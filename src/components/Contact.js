0a11bde1cd2a1d815f5e75936eba604e18234251
richinrix <richinvishal@gmail.com>
2022-03-18 01:39:26 +0530

    forms added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,78 +25,153 @@ const Contact = () => {
     //   Phone: data["Phone Number"],
     // };
     // PostData(formData);
+    setSent(true);
+
     setTimeout(() => {
-      setSent(true);
+      setSent(false);
+
       document.getElementById("contactForm").reset();
-    }, 600);
+    }, 1000);
   };
+  const Header = () => (
+    <>
+      <div class="fixed z-50 lg:hidden">
+        <div class="   lg:hidden ">
+          <HeaderAnnouncement />
+        </div>
+        <div class="  py-2 font-leagueSpartan  font-extrabold bg-gray-400 bg-opacity-40 backdrop-blur-md   text-4xl w-screen flex lg:hidden justify-center items-center ">
+          60 Cars
+        </div>
+      </div>
+      <div className="fixed bottom-0 lg:sticky  lg:top-0 z-10">
+        <div class="hidden lg:block">
+          <HeaderAnnouncement />
+        </div>
+
+        <div class=" mx-auto bg-white lg:bg-opacity-50 lg:backdrop-blur-sm w-full">
+          <Nav current="" />
+        </div>
+      </div>
+    </>
+  );
   return (
     <div className="overflow-x-clip">
       {/* nav */}
-      <div class=" font-leagueSpartan py-4 font-extrabold bg-white text-4xl w-screen flex lg:hidden justify-center items-center ">
-        60 Cars
-      </div>
-      <div className="fixed bottom-0 lg:sticky  lg:top-0 z-10">
-        <HeaderAnnouncement />
-        <div class=" mx-auto bg-white lg:bg-opacity-50 lg:backdrop-blur-sm w-full">
-          <Nav current="about" />
-        </div>
-      </div>
+      {Header()}
 
       <div class="lg:px-0 mx-auto ">
-        <div class="px-3 lg:px-0 lg:container mx-auto w-full  relative">
+        <div class="px-3 lg:px-20 py-32 lg:pt-0  mx-auto w-full  relative">
           {/* form */}
-          {/* <form
+          <form
             id="contactForm"
             onSubmit={handleSubmit(onSubmit)}
-            class="text-sm mx-auto my-10 lg:w-4/5 self-center border-2 border-black lg:p-10 "
+            class=" mx-auto   lg:w-3/5 self-center  border-2 border-black rounded-xl py-5 px-3 lg:p-10 "
           >
-            <div class="text-center text-5xl my-3 font-bold font-leagueSpartan">
+            <div class="text-center text-2xl lg:text-5xl my-3 font-bold font-leagueSpartan">
               {params.type === "company" ? "Company Sign Up" : "Contact Us"}
             </div>
-            <div className="w-full">
-              <div class="font-bold text-xl">Email Address</div>
-              <input
-                type="text my-4 border-2 border-black w-full p-2 outline-none "
-                placeholder="you@company.com"
-              />
-            </div>
-            <div>
-              <div class="font-bold">Compay Name</div>
-              <input type="text" placeholder="Company Name" />
-            </div>
-            <div class="flex w-full justify-between">
-              <div className="px-2 lg:w-1/2 ">
-                <div class="font-bold">Department</div>
-                <select
-                  className=""
+            <div class="lg:w-1/2 flex flex-col mx-auto ">
+              <div className="w-full">
+                <div class="font-bold my-2 ">Email Address</div>
+                <input
+                  type="text"
+                  className="border-2 border-black w-full py-1 px-2  rounded-md "
+                  placeholder="you@company.com"
+                />
+              </div>
+              <div>
+                <div class="font-bold my-2">Compay Name</div>
+                <input
+                  type="text"
+                  placeholder="Company Name"
+                  className="outline-none border-2 w-full border-black rounded-md px-2 py-1"
+                />
+              </div>
+              <div class="flex w-full justify-between my-2">
+                <div className=" pr-1 lg:w-1/2 ">
+                  <div class="font-bold">Department</div>
+                  <select
+                    className="outline-none border-2 w-full  border-black rounded-md px-2 py-1"
+                    name=""
+                    id=""
+                    // onChange={(e) => setCarType(e.target.value)}
+                  >
+                    <option value="Standard">Admin</option>
+                    <option value="Sedan">Travel Desk</option>
+                    <option value="MUV">Finance</option>
+                    <option value="Premium">HR</option>
+                    <option value="Luxury">Others</option>
+                  </select>
+                </div>
+                <div className=" pl-1 lg:w-1/2 ">
+                  <div class="font-bold">Number of Employees</div>
+                  <select
+                    className="outline-none border-2 w-full border-black rounded-md px-2 py-1"
+                    name=""
+                    id=""
+                    // onChange={(e) => setCarType(e.target.value)}
+                  >
+                    <option value="Standard">Less than 50</option>
+                    <option value="Sedan">5 to 200</option>
+                    <option value="MUV">200-2000</option>
+                    <option value="Premium">More than 2000</option>
+                  </select>
+                </div>
+              </div>
+              <div className="w-full my-2 ">
+                <div class="font-bold">Enter the State</div>
+                <input
+                  className="outline-none border-2 w-full border-black rounded-md px-2 py-1"
+                  placeholder="State"
                   name=""
                   id=""
                   // onChange={(e) => setCarType(e.target.value)}
-                >
-                  <option value="Standard">Admi</option>
-                  <option value="Sedan">Travel Desk</option>
-                  <option value="MUV">Finance</option>
-                  <option value="Premium">HR</option>
-                  <option value="Luxury">Others</option>
-                </select>
+                />
               </div>
-            </div>
-            <div className="px-2 lg:w-1/2 ">
-              <div class="font-bold">Number of Employees</div>
-              <select
-                className=""
-                name=""
-                id=""
-                // onChange={(e) => setCarType(e.target.value)}
+              <div className="w-full my-2">
+                <div class="font-bold">Mobile Number</div>
+                <input
+                  className="outline-none border-2 w-full border-black rounded-md px-2 py-1"
+                  type="number"
+                  placeholder="Phone Number"
+                  name=""
+                  id=""
+                  // onChange={(e) => setCarType(e.target.value)}
+                />
+                <div class="text-xs">
+                  We wil get in contact with you on this number.
+                </div>
+              </div>
+              {params.type === "general" && (
+                <div className="w-full my-2">
+                  <div class="font-bold">Message</div>
+                  <textarea
+                    rows={3}
+                    className="outline-none border-2 w-full border-black rounded-md px-2 py-1"
+                    type="number"
+                    placeholder="Enter your mesage here"
+                    name=""
+                    id=""
+                    // onChange={(e) => setCarType(e.target.value)}
+                  />
+                  <div class="text-xs">
+                    We wil get in contact with you on this number.
+                  </div>
+                </div>
+              )}
+              <div
+                onClick={onSubmit}
+                class=" hover:bg-gray-600 cursor-pointer w-full text-center rounded-md mx-auto my-2 bg-black text-white px-3 py-2 font-bold"
               >
-                <option value="Standard">Less than 50</option>
-                <option value="Sedan">5 to 200</option>
-                <option value="MUV">200-2000</option>
-                <option value="Premium">More than 2000</option>
-              </select>
+                Submit
+              </div>
+              {sent && (
+                <div class="bg-green-400 rounded-md px-2 py-1 text-sm text-center mx-auto">
+                  Submitted Successfully
+                </div>
+              )}
             </div>
-          </form> */}
+          </form>
         </div>
       </div>
     </div>
